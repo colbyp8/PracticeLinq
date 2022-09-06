2e30ce243ab21fb7af25332212b8a4e593025d62
Colby Pate <cbpate08@gmail.com>
2022-09-06 16:02:42 -0500

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gameList = new List<string>() { "Call of Duty: Black Ops", "Super Mario Kart", "Pokemon", "Mortal Kombat" , "Madden" };
+            //gameList.Add("Call of Duty: Black Ops");
+            //gameList.Add("Super Mario Kart");
+            //gameList.Add("Pokemon");
+            //gameList.Add("Mortal Kombat");
+            //gameList.Add("Madden");
+
+            gameList.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
